3291165f82fbb70465361d276e5aecc09a9b5137
Laura Zanella <zclaurap@gmail.com>
2023-11-14 12:45:07 -0800

    Homework

--- traversing.js
+++ traversing.js
@@ -1,11 +1,19 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
 const body = document.querySelector('body');
+const main = body.getElementsByTagName('main')
+console.log(main[0].innerHTML)
+
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
-
+for (let index = 0; index < ul.childElementCount; index++) {
+    const li = ul.children[index];
+    console.log(li.innerHTML)
+}
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
 const p = document.querySelector('p');
+const previousToP= p.previousElementSibling
+console.log(previousToP.children[2].innerHTML)
--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,9 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+const table = document.getElementsByTagName('table')[0]
+table.addEventListener('click', (e) => {
+    if ( e.target.localName === 'td') {
+        e.target.innerHTML = `Position: \n ${e.x}, ${e.y}`
+    }
+})
